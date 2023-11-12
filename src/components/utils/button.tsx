c376cbfb72bafb238fdd01e9dd8056618a0c1bb4
Josh Daniel <joshxfi.dev@gmail.com>
2023-11-12 12:37:21 +0800

    chore: add animation on button

--- src/components/utils/button.tsx
+++ src/components/utils/button.tsx
@@ -15,14 +15,14 @@ export function Button({ onClick, link, children, ...rest }: Props) {
       {link ? (
         <Link
           href="/tool"
-          className="border-2 text-secondary-200 absolute w-full h-full -mt-1 -ml-1 border-secondary-200 bg-white rounded text-sm md:text-base grid place-items-center"
+          className="border-2 text-secondary-200 absolute w-full h-full -mt-1 -ml-1 border-secondary-200 bg-white rounded text-sm md:text-base grid place-items-center hover:-mt-[2px] hover:-ml-[2px] transition-all"
         >
           {children}
         </Link>
       ) : (
         <button
           onClick={onClick}
-          className="border-2 text-secondary-200 absolute w-full h-full -mt-1 -ml-1 border-secondary-200 bg-white rounded text-sm md:text-base grid place-items-center disabled:mt-0 disabled:ml-0 transition-all"
+          className="border-2 text-secondary-200 absolute w-full h-full -mt-1 -ml-1 border-secondary-200 bg-white rounded text-sm md:text-base grid place-items-center hover:-mt-[2px] hover:-ml-[2px] transition-all"
           {...rest}
         >
           {children}
