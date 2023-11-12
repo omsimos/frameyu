6578aa893800676a60213f8b3038dfac919f7e8a
Josh Daniel <joshxfi.dev@gmail.com>
2023-11-12 12:32:42 +0800

    chore: add link to home on logo

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,11 +1,15 @@
+import Link from "next/link";
 import { Button } from "./utils/button";
 
 export function Navbar() {
   return (
     <nav className="py-12 flex justify-between items-center">
-      <h1 className="text-2xl font-black text-secondary-200 tracking-[-0.09em]">
+      <Link
+        href="/"
+        className="text-2xl font-black text-secondary-200 tracking-[-0.09em]"
+      >
         frame<span className="tracking-tighter text-primary-100">dip</span>
-      </h1>
+      </Link>
 
       <div className="font-medium flex space-x-24 items-center">
         <ul className="lg:flex space-x-28 hidden">
