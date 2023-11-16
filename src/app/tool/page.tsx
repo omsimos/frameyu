a9a16ceceec3d55ebd2705bcda9e458f1f5484ca
Josh Daniel <joshxfi.dev@gmail.com>
2023-11-16 15:15:51 +0800

    feat: add tailwind merge

--- src/app/tool/page.tsx
+++ src/app/tool/page.tsx
@@ -150,6 +150,7 @@ export default function Home() {
             <Button
               disabled={!frame}
               onClick={() => profileRef.current?.click()}
+              className="w-full"
             >
               Upload Photo
             </Button>
--- src/components/utils/button.tsx
+++ src/components/utils/button.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { twMerge } from "tailwind-merge";
 
 type Props = {
   onClick?: () => void;
@@ -10,20 +11,26 @@ type Props = {
   HTMLButtonElement
 >;
 
-export function Button({ onClick, link, children, ...rest }: Props) {
+export function Button({ onClick, link, className, children, ...rest }: Props) {
   return (
     <div className="bg-secondary-200 relative rounded h-[45px] w-[150px]">
       {link ? (
         <Link
           href="/tool"
-          className="border-2 text-secondary-200 absolute w-full h-full -mt-1 -ml-1 border-secondary-200 bg-white rounded text-sm md:text-base grid place-items-center hover:-mt-[2px] hover:-ml-[2px] transition-all"
+          className={twMerge(
+            "border-2 text-secondary-200 absolute w-full h-full -mt-1 -ml-1 border-secondary-200 bg-white rounded text-sm md:text-base grid place-items-center hover:-mt-[2px] hover:-ml-[2px] transition-all",
+            className
+          )}
         >
           {children}
         </Link>
       ) : (
         <button
           onClick={onClick}
-          className="border-2 text-secondary-200 absolute w-full h-full -mt-1 -ml-1 border-secondary-200 bg-white rounded text-sm md:text-base grid place-items-center hover:-mt-[2px] hover:-ml-[2px] transition-all"
+          className={twMerge(
+            "border-2 text-secondary-200 absolute w-full h-full -mt-1 -ml-1 border-secondary-200 bg-white rounded text-sm md:text-base grid place-items-center hover:-mt-[2px] hover:-ml-[2px] transition-all",
+            className
+          )}
           {...rest}
         >
           {children}
