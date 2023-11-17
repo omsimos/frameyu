32672f33e3206e4688b4768ef00185e53dccc199
Josh Daniel <joshxfi.dev@gmail.com>
2023-11-17 12:25:18 +0800

    chore: update steps section in mobile

--- src/components/steps.tsx
+++ src/components/steps.tsx
@@ -8,10 +8,10 @@ export function Steps() {
         sub="Add a frame to your photo in just 3 quick steps!"
       />
 
-      <div className="flex justify-between mt-16 lg:space-x-8 md:space-x-4 flex-col md:flex-row max-w-md mx-auto md:max-w-full space-y-12 md:space-y-0">
-        <Step num={1} desc="Upload the 1x1 PNG image of your frame" />
-        <Step num={2} desc="Add your photo to display behind the frame" />
-        <Step num={3} desc="Adjust your photo, and click download" />
+      <div className="flex justify-between mt-16 lg:space-x-8 md:space-x-4 flex-col md:flex-row max-w-md mx-auto md:max-w-full space-y-8 md:space-y-0">
+        <Step num={1} desc="Upload the 1x1 PNG image of your frame." />
+        <Step num={2} desc="Add your photo to display behind the frame." />
+        <Step num={3} desc="Adjust your photo, and click download." />
       </div>
     </section>
   );
@@ -19,13 +19,13 @@ export function Steps() {
 
 function Step({ num, desc }: { num: number; desc: string }) {
   return (
-    <div className="border-[3px] border-secondary-200 rounded-md p-8 pb-16 text-center bg-white relative">
-      <h1 className="text-6xl text-primary-100 font-extrabold">{num}</h1>
-      <p className="font-medium mt-2 sm:mt-4 md:text-lg lg:text-xl text-secondary-200">
+    <div className="border-[3px] border-secondary-200 rounded-md p-8 lg:pb-16 text-left md:text-center bg-white relative md:block flex">
+      <h1 className="lg:text-6xl text-5xl text-primary-100 font-extrabold mr-10 md:mr-0">{num}</h1>
+      <p className="font-medium md:mt-4 md:text-lg lg:text-xl text-secondary-200">
         {desc}
       </p>
 
-      <div className="absolute h-8 w-full rounded-b-md left-0 -bottom-3 -z-10 bg-secondary-200"></div>
+      <div className="absolute h-8 w-full rounded-b-md left-0 md:-bottom-3 -bottom-2 -z-10 bg-secondary-200"></div>
     </div>
   );
 }
