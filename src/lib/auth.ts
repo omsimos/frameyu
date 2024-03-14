7af8f71754b0260c337a1f4a78bb829c0a6840fc
Josh Daniel <joshxfi.dev@gmail.com>
2024-03-14 23:09:35 +0800

    feat: add validate request function

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,7 @@
-import { Lucia } from "lucia";
+import { cache } from "react";
+import { Lucia, Session, User } from "lucia";
 import { Google } from "arctic";
+import { cookies } from "next/headers";
 import { PrismaAdapter } from "@lucia-auth/adapter-prisma";
 
 export const google = new Google(
@@ -26,6 +28,41 @@ export const lucia = new Lucia(adapter, {
     };
   },
 });
+
+export const validateRequest = cache(
+  async (): Promise<
+    { user: User; session: Session } | { user: null; session: null }
+  > => {
+    const sessionId = cookies().get(lucia.sessionCookieName)?.value ?? null;
+    if (!sessionId) {
+      return {
+        user: null,
+        session: null,
+      };
+    }
+
+    const result = await lucia.validateSession(sessionId);
+    try {
+      if (result.session && result.session.fresh) {
+        const sessionCookie = lucia.createSessionCookie(result.session.id);
+        cookies().set(
+          sessionCookie.name,
+          sessionCookie.value,
+          sessionCookie.attributes,
+        );
+      }
+      if (!result.session) {
+        const sessionCookie = lucia.createBlankSessionCookie();
+        cookies().set(
+          sessionCookie.name,
+          sessionCookie.value,
+          sessionCookie.attributes,
+        );
+      }
+    } catch {}
+    return result;
+  },
+);
 
 declare module "lucia" {
   interface Register {
