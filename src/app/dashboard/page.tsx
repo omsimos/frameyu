5b7606ffd3a2087684a676cc67d2c19a65081cee
Josh Daniel <joshxfi.dev@gmail.com>
2024-03-22 12:54:14 +0800

    feat: setup urql

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,18 @@
 import { FrameCard } from "./components/frame-card";
+import { cacheExchange, createClient, fetchExchange, gql } from "@urql/core";
+import { registerUrql } from "@urql/next/rsc";
+
+const makeClient = () => {
+  return createClient({
+    url:
+      process.env.NODE_ENV === "production"
+        ? process.env.GRAPHQL_URL!
+        : "http://localhost:3000/api/graphql",
+    exchanges: [cacheExchange, fetchExchange],
+  });
+};
+
+const { getClient } = registerUrql(makeClient);
 
 export default function DashboardPage() {
   return (
