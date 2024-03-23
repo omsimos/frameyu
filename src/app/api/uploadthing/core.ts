48c00c88106587162d319833f4f939897d7ae650
Josh Daniel <joshxfi.dev@gmail.com>
2024-03-23 14:13:55 +0800

    feat: setup uploadthing api

---
+++ src/app/api/uploadthing/core.ts
@@ -0,0 +1,24 @@
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { UploadThingError } from "uploadthing/server";
+import { getSession } from "@/lib/auth";
+
+const f = createUploadthing();
+
+export const ourFileRouter = {
+  imageUploader: f({ image: { maxFileSize: "4MB" } })
+    .middleware(async () => {
+      const { user } = await getSession();
+
+      if (!user) throw new UploadThingError("Unauthorized");
+
+      return { userId: user.id };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      console.log("Upload complete for userId:", metadata.userId);
+      console.log("file url", file.url);
+
+      return { uploadedBy: metadata.userId };
+    }),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
