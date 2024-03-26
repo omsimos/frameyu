63ca2c94f5b53e817d9b88dcade876cfd2a8fa2b
Josh Daniel <joshxfi.dev@gmail.com>
2024-03-26 14:18:59 +0800

    feat: setup turso db with prisma

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,7 +1,15 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaLibSQL } from "@prisma/adapter-libsql";
+import { createClient } from "@libsql/client";
+
+const libsql = createClient({
+  url: process.env.TURSO_DATABASE_URL!,
+  authToken: process.env.TURSO_AUTH_TOKEN,
+});
 
 const prismaClientSingleton = () => {
-  return new PrismaClient();
+  const adapter = new PrismaLibSQL(libsql);
+  return new PrismaClient({ adapter });
 };
 
 declare global {
