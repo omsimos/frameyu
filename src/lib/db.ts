9595349ca26e412aca476f79470b15e336a2a2f9
Josh Daniel <joshxfi.dev@gmail.com>
2024-03-26 14:44:58 +0800

    fix: use string for libsql client opts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -3,8 +3,8 @@ import { PrismaLibSQL } from "@prisma/adapter-libsql";
 import { createClient } from "@libsql/client";
 
 const libsql = createClient({
-  url: process.env.TURSO_DATABASE_URL!,
-  authToken: process.env.TURSO_AUTH_TOKEN,
+  url: `${process.env.TURSO_DATABASE_URL}`,
+  authToken: `${process.env.TURSO_AUTH_TOKEN}`,
 });
 
 const prismaClientSingleton = () => {
