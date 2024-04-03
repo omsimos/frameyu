7b984c56a0daf35ee9b35e4b1aca71f9e9e9312a
Josh Daniel <joshxfi.dev@gmail.com>
2024-04-03 10:03:55 +0800

    fix: redirect user to login from dashboard if no session

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,15 +1,23 @@
 import type { Metadata } from "next";
 import { DashboardNavbar } from "./components/navbar";
+import { getSession } from "@/lib/auth";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "Frameyu | Dashboard",
 };
 
-export default function DashboardLayout({
+export default async function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const { session } = await getSession();
+
+  if (!session) {
+    redirect("/login");
+  }
+
   return (
     <div className="min-h-screen">
       <DashboardNavbar />
