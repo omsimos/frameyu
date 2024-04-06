f8ae320eff4365d7cb4cd6fede682de3ca7528e0
Josh Daniel <joshxfi.dev@gmail.com>
2024-04-06 20:59:54 +0800

    feat: add native share method in button

--- src/app/f/[handle]/components/share-button.tsx
+++ src/app/f/[handle]/components/share-button.tsx
@@ -1,18 +1,28 @@
-"use client"
+"use client";
 
 import { Button } from "@/components/ui/button";
 import { Share2 } from "lucide-react";
 import { toast } from "sonner";
 
 export function ShareButton({ handle }: { handle: string }) {
-  const copyLink = () => {
+  const share = () => {
     if (typeof window !== "undefined") {
+      const url = `${window.location.origin}/f/${handle}`;
       navigator.clipboard.writeText(`${window.location.origin}/f/${handle}`);
       toast.success("Copied to clipboard");
+
+      if (
+        navigator.share &&
+        navigator.canShare({ url }) &&
+        process.env.NODE_ENV === "production"
+      ) {
+        navigator.share({ url });
+      }
     }
   };
+
   return (
-    <Button size='icon' variant="secondary" onClick={copyLink}>
+    <Button size="icon" variant="secondary" onClick={share}>
       <Share2 className="h-4 w-4" />
     </Button>
   );
